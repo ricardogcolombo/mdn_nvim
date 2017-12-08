e06a44c2cc6593a6e77a28e98a9527e38ccc8c6a
ricardogcolombo <ricardogcolombo@gmail.com>
2017-12-08 09:14:52 -0300

    [ADD]new buffer function

--- rplugin/node/mdn_nvim/src/index.js
+++ rplugin/node/mdn_nvim/src/index.js
@@ -11,20 +11,27 @@ class mdnPlugin {
     async longCommand(args) {
         console.log('Output will be routed to $NVIM_NODE_LOG_FILE');
         const bufferName = await this.nvim.buffer.name;
+        this.nvim.command('vnew');
         return bufferName;
+    }
+
+    @Function('Vsplit', { sync: true })
+    splitMe(args, done) {
+        this.nvim.command('vsplit');
     }
 
     @Command('TigrisSSS')
     async clear() {
         const buffer = await this.nvim.buffer;
         this.nvim.outWrite('Clearing\n');
+
         buffer.clearHighlight({ srcId: -1 });
         DEBUG_MAP.clear();
     }
 
     @Command('UsePromises')
     promiseExample() {
-        return this.nvim.buffer.insert(['test','test2'],2).then((buff) => {
+        return this.nvim.buffer.append(['test','test2'],2).then((buff) => {
             console.log(`Current buffer name is ${name}`);
         });
     }
